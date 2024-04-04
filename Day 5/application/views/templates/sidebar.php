978bc88c83f85d0f44802ded3bd27a96d0b2b8e2
BivekChaudhary876 <bivekchaudhary798@gmail.com>
2024-04-05 00:32:07 +0545

    Day 5 task is added

---
+++ Day 5/application/views/templates/sidebar.php
@@ -0,0 +1,36 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">Sidebar</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <!-- <li class="nav-item">
+        <a href="index.php?c=dashboard" class="nav-link" aria-current="page">
+          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+          Home
+        </a>
+      </li> -->
+      <li>
+        <a href="index.php?c=dashboard" class="nav-link link-body-emphasis">
+          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+          Dashboard
+        </a>
+      </li>
+      <li>
+        <a href="index.php?c=user&m=list" class="nav-link link-body-emphasis">
+          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#people-circle"></use></svg>
+          Users
+        </a>
+      </li>
+    </ul>
+  </div>
+
+<!-- <div class="sidebar">
+    <div class="sidebar-content">
+        <ul>
+            <li><a href="index.php?c=dashboard">Dashboard</a></li>
+            <li><a href="index.php?c=user&m=list">Users</a></li>
+        </ul>
+    </div>
+</div> -->
