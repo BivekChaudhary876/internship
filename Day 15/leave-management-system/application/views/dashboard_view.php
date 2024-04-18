6e6936b871a18fd03e2597466e8c1c0ebc18b600
BivekChaudhary876 <bivekchaudhary798@gmail.com>
2024-04-18 18:02:50 +0545

    Day 15 task is added

---
+++ Day 15/leave-management-system/application/views/dashboard_view.php
@@ -0,0 +1,46 @@
+<?php 
+$role = $_SESSION['current_user']['role']; 
+?>
+
+<div class="my-3">
+    <?php if($role == 'admin'): ?>
+        <div class="my-3 text-center d-flex justify-content-evenly">
+            <!-- Form to select username -->
+            <form method="post">
+                <select name="selected_user" id="usernameDropdown" class="p-1 btn border-success rounded-start rounded-end">
+                    <option value="0"><?php echo 'Select User' ?></option>
+                    <?php foreach($usernames as $user): ?>
+                        <option value="<?= $user ?>"><?= $user ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <input type="submit" value="Submit" class="p-1 btn btn-outline-success">
+            </form>
+        </div>
+
+        <!-- Display leave requests for the selected user -->
+        <?php if(isset($selectedUserLeaveRequests) && !empty($selectedUserLeaveRequests)): ?>
+            <div class="my-3 text-center">
+                    <h2>Leave Requests for <?= $selected_user ?></h2>
+                    <h3>Total Leave Days :<?= $totalLeaveDays ?></h3>
+            </div>
+            <div class="my-3 text-center d-flex justify-content-evenly">
+                <button id="totalLeaveBtn" class="btn btn-outline-info">Total Leaves<br><?= $totalLeaveCount ?></button>
+                <button id="pendingBtn" class="btn btn-outline-warning">Pending<br><?= $pendingLeaveCount ?></button>
+                <button id="approvedBtn" class="btn btn-outline-success">Approved<br><?= $approvedLeaveCount ?></button>
+                <button id="rejectedBtn" class="btn btn-outline-danger">Rejected<br><?= $rejectedLeaveCount ?></button>
+            </div>
+        <?php endif; ?>
+
+    <?php elseif($role == 'user'): ?>
+        <div class="text-center my-3">
+            <h2>Leave Requests</h2>
+            <h3>Total Leave Days :<?= $totalLeaveDays ?></h3>
+        </div>
+        <div class="my-3 text-center d-flex justify-content-evenly">
+            <button id="totalLeaveBtn" class="btn btn-outline-info">Total Leaves<br><?= $totalLeaveCount ?></button>
+            <button id="pendingBtn" class="btn btn-outline-warning">Pending<br><?= $pendingLeaveCount ?></button>
+            <button id="approvedBtn" class="btn btn-outline-success">Approved<br><?= $approvedLeaveCount ?></button>
+            <button id="rejectedBtn" class="btn btn-outline-danger">Rejected<br><?= $rejectedLeaveCount ?></button>
+        </div>
+    <?php endif; ?>
+</div>
---
+++ Day 15/leave-management-system/system/base_controller.php
@@ -0,0 +1,79 @@
+<?php 
+abstract class Base_Controller{
+
+    protected $model;
+
+    protected $c; // store current controller
+
+    protected $m; // store current method
+
+    public function __construct(){
+
+        $this->c = get_current_controller();
+        $this->m = get_current_method();
+
+        // check if allowd post methods are access throught other request methods
+        if( isset( $this->post_methods ) && $_SERVER[ 'REQUEST_METHOD' ] != 'POST' && in_array( $this->m, $this->post_methods ) ){
+            redirect( 'user' );
+        }
+
+        if( $this->is_logged_in() ){
+            if( 'user' == $this->c && 'index' == $this->m ){
+                redirect( 'dashboard' );
+            }
+        }else{
+            // not logged in
+            if( !( 'user' == $this->c && ( 'index' == $this->m || 'login' == $this->m ) ) ){
+                redirect( 'user' );
+            } 
+        }
+        
+        $this->model = load_model( $this->c );
+    }
+
+    public function is_logged_in(){
+        return isset( $_SESSION[ 'current_user' ] );
+    }
+
+    public function load_view( $data = [], $view = '' ){
+
+        extract( $data );
+        require_once PATH . '/application/views/templates/head.php';
+        echo '<div class="container-fluid">';
+        echo '<div class="row">';
+        if( !( 'user' == $this->c && 'index' == $this->m ) ){
+            require_once PATH . '/application/views/templates/navbar.php';
+            echo '</div>';
+            echo '</div>';
+            echo '<div class="container">';
+            echo '<div class="row">';
+            echo '<div class="col-2">';
+            require_once PATH . '/application/views/templates/sidebar.php';
+            echo '</div>';
+        }
+        ?>
+            <div class="<?php echo(($view == 'login') ? 'col-12' : 'col-10'); ?>">
+                <?php
+                    $view = empty( $view ) ? $this->c : $view;
+                    $view_path = PATH . '/application/views/' . $view . '_view.php';
+                    if( file_exists( $view_path ) ){
+                        require $view_path;
+                    }else{
+                        dd( $view . '_view.php does not exists' ); //dunmp and die function
+                    }
+                ?>
+            </div>
+        <?php
+        echo '</div>';
+        
+        require_once PATH . '/application/views/templates/footer.php';
+        echo '</div>';
+    }
+
+    public function logout(){
+        session_start();    
+        session_destroy();
+        redirect( 'user' );
+    }
+
+}
